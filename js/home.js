8bbf903e76e6c357d596c39059696516c7d16598
Vitoria <jesusvitoriabarroso@gmail.com>
2025-05-30 12:45:09 -0300

    Primeiro commit do site

---
+++ js/home.js
@@ -0,0 +1,41 @@
+
+  const carrousel = document.getElementById("carouselInner");
+  const cards = document.querySelectorAll(".card1");
+  let index = 0;
+
+  function slideCarousel() {
+    index++;
+    if (index >= cards.length) {
+      index = 0;
+    }
+    const cardWidth = cards[0].offsetWidth + 20; // 300px + margem
+    carrousel.style.transform = `translateX(-${index * cardWidth}px)`;
+  }
+
+  setInterval(slideCarousel, 3000); // muda a cada 3 segundos
+
+
+
+// // Adiciona o evento de rolagem
+// window.addEventListener("scroll", verificarCarrossel);
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let lastScrollTop = 0;
+//     const segundaSecao = document.querySelector(".segunda_section");
+
+//     window.addEventListener("scroll", function () {
+//         let scrollTop = window.scrollY;
+
+//         if (scrollTop > lastScrollTop) {
+//             // Rolando para baixo, esconde a segunda seção
+//             segundaSecao.classList.remove("fade-out");
+//         } else {
+//             // Rolando para cima, exibe a segunda seção
+//             segundaSecao.classList.add("fade-out");
+//         }
+
+//         lastScrollTop = scrollTop;
+//     });
+// });
+
+
